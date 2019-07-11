9cca125f0efe6eab14bdc948118ba92efd689bb4
NilouferS <niloufer.shaikh11@gmail.com>
2019-07-11 09:46:31 +0100

    Updates

---
+++ precache-manifest.cac1b1ae6317a6d94f2fea25cdece54b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1bb8854805399d8c21ffe3869356c54",
+    "url": "/NPortfolio/index.html"
+  },
+  {
+    "revision": "5101aa47b6ceba825168",
+    "url": "/NPortfolio/static/css/main.71550497.chunk.css"
+  },
+  {
+    "revision": "dd924b1829cfac0fa982",
+    "url": "/NPortfolio/static/js/2.ee8ed330.chunk.js"
+  },
+  {
+    "revision": "5101aa47b6ceba825168",
+    "url": "/NPortfolio/static/js/main.bf8e29fc.chunk.js"
+  },
+  {
+    "revision": "ee90e69443693bc756a3",
+    "url": "/NPortfolio/static/js/runtime~main.f721a146.js"
+  },
+  {
+    "revision": "87d994875c22d91270c9d27302a45fb7",
+    "url": "/NPortfolio/static/media/bootstrap.87d99487.png"
+  },
+  {
+    "revision": "8d859602e4edf37f8e6fbce188479840",
+    "url": "/NPortfolio/static/media/react.8d859602.png"
+  },
+  {
+    "revision": "e895e23905faaa074aac85f50d5017a1",
+    "url": "/NPortfolio/static/media/self.e895e239.jpg"
+  },
+  {
+    "revision": "6ded14c8a9e078f1d9c305186271078d",
+    "url": "/NPortfolio/static/media/wordpress.6ded14c8.png"
+  }
+]);
