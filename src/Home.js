9d298fbd50c385582b7485ed1f7feb70de5bf1a7
NilouferS <niloufer.shaikh11@gmail.com>
2019-07-30 13:34:19 +0100

    test

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,7 @@ export default function Home() {
             <div class="containerHome">
                 <div class="animate item">
                     <span>I</span><span></span><span>a</span><span>m</span><br/>
-                    <span>N</span><span>I</span><span>L</span><span>O</span><span>U</span><span>F</span><span>E</span><span>R</span><span>.</span><span>.</span>
+                    <span></span><span>I</span><span>L</span><span>O</span><span>U</span><span>F</span><span>E</span><span>R</span><span>.</span><span>.</span>
                 </div>
             </div>
         </div>
