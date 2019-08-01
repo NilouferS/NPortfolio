2c2b6e5184bc718d5294cbb592c72fb9ec5f58f5
NilouferS <niloufer.shaikh11@gmail.com>
2019-08-02 00:57:27 +0100

    test

--- src/Home.js
+++ src/Home.js
@@ -6,10 +6,10 @@ export default function Home() {
         <div id='home'>
             <div class="containerHome">
                 <div className='containHome'>
-                    <li className='item telephone'><a href="tel:0759652551"> </a></li>
-                    <li className='item email'><a href='mailto:niloufer.shaikh11@gmail.com'> </a></li>
-                    <li className='item github'><a href='https://github.com/NilouferS'> </a></li>
-                    <li className='item linkedin'><a href='https://www.linkedin.com/in/niloufer-shaikh-476158a9/'> </a></li>
+                    <li className='item telephone'><a href="tel:0759652551">a</a></li>
+                    <li className='item email'><a href='mailto:niloufer.shaikh11@gmail.com'>b</a></li>
+                    <li className='item github'><a href='https://github.com/NilouferS'>c</a></li>
+                    <li className='item linkedin'><a href='https://www.linkedin.com/in/niloufer-shaikh-476158a9/'>d</a></li>
                 </div>
 
                 <div class="animate item">
