d873750ffc65f9567117109e9037fd554f4e6abb
NilouferS <niloufer.shaikh11@gmail.com>
2019-08-02 09:48:50 +0100

    AboutPage

--- src/About.js
+++ src/About.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import './About.css';
 
 export default function About() {
     return (
         <div id='about'>
-            <h1 className='title'>About</h1>
-            <p>ewfefergegt4whernhr</p>
+            <h1 className='title'>ABOUT</h1>
+            <div className='aboutContainer'>
+                <p>Hi. My name is Niloufer. I am ambitious and enthusiastic software developer based in London.
+                 I have experience in working with number of technology in different capacities however I have decided to
+              focus on full stack software development. My approach to work is using agile methodology.</p>
+                <p>My current experience is working with technologies such as React, JavaScript ES6, HTML5, CSS3.</p><br/>
+                <p>I have a Bacheclor of Computer Engineering degree from Pune University, India (Compared as BA HONS by UK Naraic).
+                '''''''''''' These are skills I apply on a daily basis.</p><br/>
+                <p>I have a wide range of interests in learning new technology, meeting new people and
+                '''''''''many events'.</p>
+            </div>
+
         </div>
     );
 }
